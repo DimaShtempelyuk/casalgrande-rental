838e8865d6532a7613bcd3f20100d98fce280715
DimaShtempelyuk <dimon.shtempelyuk@gmail.com>
2025-01-12 18:11:01 +0100

    Fix EmailJS bug

--- src/pages/CarDetailPageElements/CarDetailPage.js
+++ src/pages/CarDetailPageElements/CarDetailPage.js
@@ -1,10 +1,8 @@
 // CarDetailPage.js
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import emailjs from 'emailjs-com';
 import { cars } from '../../data/carData';
-import Swal from 'sweetalert2';
 import PriceTable from '../../components/PriceTable';
 import OrderForm from '../../components/CarDetailPageElements/OrderForm';
 import AutoImageCarousel from './CarDetailElements/AutoImageCarousel';
@@ -13,52 +11,6 @@ import CompactDescription from './CarDetailElements/CompactDescription';
 const CarDetailPage = () => {
   const { id } = useParams();
   const car = cars.find((car) => car.id === parseInt(id));
-  const form = useRef();
-  const [rentalDate] = useState(null);
-  const [returnDate] = useState(null);
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-  
-    const rentalDateString = rentalDate ? rentalDate.toLocaleDateString('en-GB') : '';
-    const returnDateString = returnDate ? returnDate.toLocaleDateString('en-GB') : '';
-  
-    const formData = {
-      car_name: car ? car.name : '',
-      user_name: e.target.user_name.value,
-      user_email: e.target.user_email.value,
-      user_phone: e.target.user_phone.value,
-      user_id: e.target.user_id.value,
-      rental_date: rentalDateString,
-      return_date: returnDateString,
-      abroad_trip: e.target.abroad_trip.value,
-      message: e.target.message.value,
-    };
-  
-    emailjs
-      .send('service_iggsweb', 'template_eah3lbr', formData, 'HuUqQRhtUZI0Fj6-O')
-      .then(
-        (result) => {
-          console.log(result.text);
-          Swal.fire({
-            icon: 'success',
-            title: 'Poptávka odeslána!',
-            text: 'Vaše objednávka byla úspěšně odeslána. Brzy Vás budeme kontaktovat!',
-            confirmButtonColor: '#3085d6',
-          });
-        },
-        (error) => {
-          console.log(error.text);
-          Swal.fire({
-            icon: 'error',
-            title: 'No to teda...',
-            text: 'Chyba. Zkuste později.',
-            confirmButtonColor: '#d33',
-          });
-        }
-      );
-  };
-  
 
   if (!car) return <p>Car not found.</p>;
 
@@ -69,14 +21,13 @@ const CarDetailPage = () => {
       </ImageCarouselContainer>
       
       <ContentContainer>
-          <CompactDescription car={car}/>
-
+        <CompactDescription car={car} />
 
         <AdditionalInfo>
           <PriceTable priceRanges={car.priceRanges} deposit="15 000 Kč" />
-          <br></br>
+          <br />
           <h1>Poptejte toto auto už dneska!</h1>
-          <OrderForm formRef={form} sendEmail={sendEmail} carName={car.name} />
+          <OrderForm carName={car.name} />
         </AdditionalInfo>
       </ContentContainer>
     </DetailContainer>
